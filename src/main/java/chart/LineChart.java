e062fe41fefda27109347f648d6348da25ce856f
Mayusor <mayuso666@gmail.com>
2016-01-28 12:57:30 +0100

    Implemented XYChart.

--- src/main/java/chart/LineChart.java
+++ src/main/java/chart/LineChart.java
@@ -1,70 +1,23 @@
 package chart;
 
-import java.util.ArrayList;
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
 
-import data.Data;
 
 public class LineChart extends ApplicationFrame {
-   
+
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-	public LineChart( String applicationTitle , String chartTitle, Data data, int index){
-      super(applicationTitle);
-      JFreeChart lineChart = ChartFactory.createLineChart(
-         chartTitle,
-         "Years","Number of errors",
-         createDataset(data, index),
-         PlotOrientation.VERTICAL,
-         true,true,false);
-         
-      ChartPanel chartPanel = new ChartPanel( lineChart );
-      chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
-     
-      setContentPane( chartPanel );
-     
-   }
 
-   private DefaultCategoryDataset createDataset(Data data , int index)
-   {
-	   	
-	   	boolean found=false;
-	   	ArrayList<double[]> items = new ArrayList<double[]>();
-		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-		for (int i = 1; i < data.getData().size(); i++) {
-			for(int j=0;j<items.size();j++){
-				if(Double.valueOf((String) data.getData().get(i).get(index))==items.get(j)[0]){
-					items.get(j)[1]=items.get(j)[1]+1;
-					found=true;
-				}		
-			
-			}
-			
-			if(!found){
-				try{
-					double[] newElement = new double[2];
-					newElement[0]=Double.valueOf((String) data.getData().get(i).get(index));
-					newElement[1]=1;
-					items.add(0,newElement);
-				}catch(ClassCastException e){
-					
-				}
-			}
-			found=false;
+	public LineChart(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+   
 
-		}
-		for(int i=0;i<items.size();i++){
-			dataset.setValue(items.get(i)[1], String.valueOf(items.get(i)[0]), String.valueOf(items.get(i)[0]));
-		}
-		
-		
-		return dataset; 
-   }
+	 
 
 }
 
---
+++ src/main/java/chart/XYChart.java
@@ -0,0 +1,84 @@
+package chart;
+import java.awt.BasicStroke;
+import java.awt.Color;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+
+import data.Data;
+
+
+public class XYChart extends ApplicationFrame {
+
+	   /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public XYChart( String applicationTitle, String chartTitle, String x, String y, String seriesName, Data data, int index1, int index2){
+	      super(applicationTitle);
+	      JFreeChart xylineChart = ChartFactory.createXYLineChart(
+	         chartTitle ,
+	         x ,
+	         y,
+	         createDataset(seriesName, data, index1, index2) ,
+	         PlotOrientation.VERTICAL ,
+	         true , true , false);
+	         
+	      ChartPanel chartPanel = new ChartPanel( xylineChart );
+	      chartPanel.setPreferredSize( new java.awt.Dimension( 560 , 367 ) );
+	      final XYPlot plot = xylineChart.getXYPlot( );
+	      XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer( );
+	      renderer.setSeriesPaint( 0 , Color.RED );
+	      renderer.setSeriesStroke( 0 , new BasicStroke( .5f ));
+	      plot.setRenderer( renderer ); 
+	      setContentPane( chartPanel ); 
+	   }
+	   
+	   private XYDataset createDataset( String seriesName, Data data, int index1, int index2 )
+	   {
+	      final XYSeries mySeries = new XYSeries( seriesName ); 
+	      final XYSeriesCollection dataset = new XYSeriesCollection( );
+	      for(int i=0;i<data.getData().size();i++){
+	    	  try{
+	    		  mySeries.add(Double.valueOf(String.valueOf(data.getData().get(i).get(index1))), 
+		    			  Double.valueOf(String.valueOf(data.getData().get(i).get(index2))));
+	    	  }catch(NumberFormatException e){}
+	    	  
+	      }
+	               
+	       
+	      dataset.addSeries( mySeries );
+	      
+	      
+	      //You can add more than one series at a time.
+	      
+	      /*final XYSeries chrome = new XYSeries( "Chrome" );          
+	      chrome.add( 1.0 , 4.0 );          
+	      chrome.add( 2.0 , 5.0 );          
+	      chrome.add( 3.0 , 6.0 );          
+	      final XYSeries iexplorer = new XYSeries( "InternetExplorer" );          
+	      iexplorer.add( 3.0 , 4.0 );          
+	      iexplorer.add( 4.0 , 5.0 );          
+	      iexplorer.add( 5.0 , 4.0 );         
+	              
+	      dataset.addSeries( chrome );          
+	      dataset.addSeries( iexplorer );*/ 
+	      return dataset;
+	   }
+
+	 
+	  
+	   
+}
+
+
+
