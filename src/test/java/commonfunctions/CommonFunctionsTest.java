f24dce9c7ffc3038134a6a25f46eed3ecaf44119
Mayusor <mayuso666@gmail.com>
2016-02-11 15:43:07 +0100

    Added Test folders, structure and first tests to the project.

---
+++ src/test/java/commonfunctions/CommonFunctionsTest.java
@@ -0,0 +1,29 @@
+package commonfunctions;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CommonFunctionsTest {
+
+	CommonFunctions cf;
+	@Before
+	public void initialize(){
+		 cf = new CommonFunctions();  
+	}
+	@Test
+	public void testNumber() {
+        assertTrue(cf.isNumeric("4"));  
+	}
+	@Test
+	public void testCharacter() { 
+        assertFalse(cf.isNumeric("a"));  
+	}
+	@Test
+	public void testString() { 
+     
+        assertFalse(cf.isNumeric("Hello World"));  
+	}
+
+}
